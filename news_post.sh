d7644d930abf2d2da986a143efd5630ee08929ad
User Xxxx <info@aja0.com>
2021-03-06 04:02:47 +0900

    sleep

--- news_post.sh
+++ news_post.sh
@@ -4,5 +4,7 @@ cd `dirname $0`
 
 /usr/local/bin/curl --silent https://m-league.aja0.com?update_feedwordpress=1 2>&1 1>/dev/null
 
+sleep 10 2>&1 1>/dev/null
+
 /usr/local/bin/php ./wp/news_post.php 2>&1 1>/dev/null
 
