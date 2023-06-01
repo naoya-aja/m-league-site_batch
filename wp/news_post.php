059a5eeb894e248a5f5da8d254d9a7bc3bef3a87
User Xxxx <info@aja0.com>
2023-06-01 15:42:03 +0900

    アイキャッチ画像設定 LIB化

--- wp/news_post.php
+++ wp/news_post.php
@@ -9,6 +9,7 @@
  */
 // /home/xxxx/batch/test
 // /home/xxxx/www/m-league/
+require_once(__DIR__ . '/lib.php');
 require_once('/home/xxxx/www/m-league/wp-load.php');
 echo "start ****\n";
 
@@ -25,30 +26,6 @@ $args_org = array(
 	'author' => 3,	// ABEMA TIMES
 	'posts_per_page' => -1,	// ページングなし
 );
-
-// アイキャッチ画像設定
-function set_featured_image($post_id, $tmp_path, $ext) {
-	$filename = pathinfo($tmp_path, PATHINFO_FILENAME) . '.' . $ext;
-	$upload_dir = wp_upload_dir();
-	if (wp_mkdir_p($upload_dir['path'])) {
-		$file = $upload_dir['path'] . '/' . $filename;
-	} else {
-		$file = $upload_dir['basedir'] . '/' . $filename;
-	}
-	rename($tmp_path, $file);
-
-	$filetype = wp_check_filetype( $filename, null );
-	$attachment = array(
-		'post_mime_type' => $filetype['type'],
-		'post_title'     => sanitize_file_name( $filename ),
-		'post_content'   => '',
-		'post_status'    => 'inherit'
-	);
-	$attach_id = wp_insert_attachment( $attachment, $file, $post_id );
-	$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
-	wp_update_attachment_metadata( $attach_id, $attach_data );
-	set_post_thumbnail( $post_id, $attach_id );
-}
 
 $keywords = ['Mリーグ', 'Mリーガー'];
 foreach ($keywords as $keyword) {
